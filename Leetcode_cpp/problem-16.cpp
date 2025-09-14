1fb2bfad08fe62a6a45d8aaff507836ba07721df
AnshullKumar <anshustudy21@gmail.com>
2025-09-14 15:57:33 +0530

    new commit

---
+++ Leetcode_cpp/problem-16.cpp
@@ -0,0 +1,48 @@
+/* Leetcode Problem: 3 Sum Closest
+
+Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+*/
+
+// Solution
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int closest = nums[0] + nums[1] + nums[2];
+        for(int i = 0; i < n - 2; i++){
+            int j = i + 1;
+            int k = n - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(abs(sum - target) < abs(closest - target)){
+                    closest = sum;
+                }
+                if(sum < target) j++;    
+                else if(sum > target) k--;
+                else return sum; 
+            }
+        }return closest;
+    }
+};
